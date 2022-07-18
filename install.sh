2d3d50a3477c4a92fa80f52ba9c7767b038f551f
Kevin Dangoor <dangoor@github.com>
2022-07-18 12:47:56 -0400

    Don't install Homebrew automatically
    It makes Codespaces slow to start up.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 if ! which brew &>/dev/null; then
-  echo "Installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Homebrew is missing. Please install it first."
+  #echo "Installing Homebrew..."
+  #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew install chezmoi
