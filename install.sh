65816655981e2c0348ac28be5c815d8dd4081926
Kevin Dangoor <dangoor@github.com>
2025-12-31 17:18:14 -0500

    Change the way chezmoi runs

--- install.sh
+++ install.sh
@@ -29,5 +29,11 @@ else
   exit 1
 fi
 
-echo "Applying chezmoi configuration..."
-chezmoi apply
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+set -- init --apply --source="${script_dir}"
+
+echo "Running 'chezmoi $*'" >&2
+# exec: replace current process with chezmoi
+exec "$chezmoi" "$@"
